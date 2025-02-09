1805dc658b8060afb6f0c2d6948f67f3f441094a
evlc <dodgedodgedoggy@gmail.com>
2025-02-09 17:38:17 +0700

    Spearfish: execCommand

---
+++ src/Spearfish.hs
@@ -0,0 +1,9 @@
+module Spearfish where
+
+import System.IO
+import System.Process
+
+execCommand :: String -> [String] -> IO (Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle)
+execCommand cmd args = do
+  let proccess = (proc cmd args) {std_out = CreatePipe}
+  createProcess proccess
