e6e3fa2e71a86b89a345de9282b39b45d09f512a
evlc <dodgedodgedoggy@gmail.com>
2025-02-09 17:53:21 +0700

    Spearfish: oneline execCommand

--- src/Spearfish.hs
+++ src/Spearfish.hs
@@ -4,5 +4,4 @@ import System.IO
 import System.Process
 
 execCommand :: String -> [String] -> IO (Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle)
-execCommand cmd args = do
-  createProcess (proc cmd args)
+execCommand cmd args = createProcess (proc cmd args)
