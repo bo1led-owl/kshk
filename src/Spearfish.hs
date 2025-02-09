885fd033c5588402998d7dab976f6589bd2d2cc5
rendemare <rendemare@gmail.com>
2025-02-09 18:27:06 +0700

    bez mata 2

--- src/Spearfish.hs
+++ src/Spearfish.hs
@@ -3,5 +3,8 @@ module Spearfish where
 import System.IO
 import System.Process
 
-execCommand :: String -> [String] -> IO (Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle)
-execCommand cmd args = createProcess (proc cmd args)
+execCommand :: String -> [String] -> IO String
+execCommand cmd args = do
+  let proccess = (proc cmd args) {std_out = CreatePipe}
+  (Just inp, Just out, Just err, phandle) <- createProcess proccess
+  hGetContents out
