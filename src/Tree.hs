d7ae6dc4f7617a8eeb98d0a1e5965dfff95a2a55
rendemare <rendemare@gmail.com>
2025-02-10 20:09:53 +0700

    Shit i've been on

--- src/Tree.hs
+++ src/Tree.hs
@@ -1,9 +1,20 @@
 module Tree where
 
+import qualified Data.Map as M
+
+data Def
+  = VarDef String Expr
+  | FuncDef String [String] Expr
+
+data Statement = E Expr | D Def
+
 data Expr
-  = Def String [String] Expr
-  | Exec Expr [Expr]
-  | SLit String
-  | FLit String
-  | PLit String
-  deriving (Show)
+  = FuncCall String [Expr]
+  | ProcCall String [Expr]
+  | VarRef String
+  | StrLit String
+
+data EState = State
+  { vars :: M.Map String Expr,
+    funcs :: M.Map String ([String], Expr)
+  }
